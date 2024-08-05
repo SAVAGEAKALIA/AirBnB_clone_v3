b78843af677d278f52a13ded4bb1920c3e25c4e5
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-08-05 11:51:57 +0100

    Air_bnbv3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -77,7 +77,15 @@ class FileStorage:
             return None
 
         if cls is not None and id is not None:
-            key = cls.__name__ + '.' + id
+            if isinstance(cls, str):
+                # cls = self.classes.get(cls)
+                if self.classes.get(cls) is None:
+                    # cls = globals()[cls]
+                    return None
+                else:
+                    cls = self.classes.get(cls)
+
+            key = f"{cls.__name__}.{id}"
             # return self.__objects[key]
             return self.__objects.get(key)
 
